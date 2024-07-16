116948843caf250c939f821c1fbe778f5997ccba
Alex Nascimento <alexjose68@hotmail.com>
2024-07-16 11:33:46 -0300

    feat: instalação da lib Swiper para efeito de slide, configuração do Swiper em main.tsx e pré-configuração do swiper no componente Banners criando um array de objetos para ser percorrido e renderizando por um map. Também adicionei mais uma imagem em assets.

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,15 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './App';
 import { Toaster } from 'react-hot-toast';
 import './index.css'
+import { register } from 'swiper/element/bundle';
 
 import CartProvider from './contexts/CartContext';
+
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Home/sections/Banners/index.tsx
+++ src/pages/Home/sections/Banners/index.tsx
@@ -1,15 +1,43 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import dog_and_cat from '../../../../assets/seu-amiguinho-pet.png';
+import dog_cat_bird from '../../../../assets/cachorrogatocalopsita.png';
+import agende from '../../../../assets/ligue-ja-pet.png';
+
+interface BannerProps {
+  id: string;
+  title: string;
+  image: string;
+  color?: string;
+}
+
+const banners: BannerProps[] = [
+  {id: '1', title: 'Cuide do seu melhor amigo com o que há de melhor! Confira nossas ofertas e produtos exclusivos para o seu pet!', image: dog_and_cat, color: "#81AAB8"},
+  {id: '2', title: 'Temos serviço veterinário 24 horas, com profissionais experientes para melhor atender seu amiguinho!', image: dog_cat_bird, color: "#854C9D"},
+  {id: '3', title: 'Com serviço a domicílio, vamos até seu pet e damos toda atenção e comodiadade necessária! Agende já!', image: agende, color: "#FF7A00"},
+]
 
 export function Banners() {
   return (
-    <section className="flex w-full max-w-7xl flex-col sm:flex-row mx-auto max-h-[600px] text-center max-sm:mt-[100px] bg-[#81AAB8] py-10 sm:py-4">
-      <h1 className='flex flex-1 font-bold text-gray-900 text-2xl md:text-3xl lg:text-4xl sm:text-left mx-5 lg:mx-10 items-center justify-center'>
-        Cuide do seu melhor amigo com o que há de melhor! Confira nossas ofertas e produtos exclusivos para o seu pet!
-      </h1>
-      <div>
-        <img className='flex flex-1 mx-auto lg:mx-10' src={dog_and_cat} alt="" />
-      </div>
-    </section>
+    <Swiper
+      slidesPerView={1}
+      navigation={false}
+      pagination
+      autoplay={{delay: 6000}}
+    >
+      {banners.map((item) => (
+        <SwiperSlide key={item.id}>
+        <section className={`flex w-full max-w-7xl flex-col sm:flex-row mx-auto max-h-[600px] text-center max-sm:mt-[100px] py-10 sm:py-4`} style={{ backgroundColor: item.color }}>
+          <h2 className='flex flex-1 font-bold text-gray-900 text-2xl md:text-3xl lg:text-4xl sm:text-left mx-5 lg:mx-10 items-center justify-center'>
+            {item.title}
+          </h2>
+          <div>
+            <img className='flex flex-1 mx-auto lg:mx-10 max-h-96' src={item.image} alt="" />
+          </div>
+        </section>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
 
