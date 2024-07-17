8b87f3f282cc432b8733cd9e109e3c597700b72e
Alex Nascimento <alexjose68@hotmail.com>
2024-07-16 21:55:03 -0300

    feat: criei a lógica para o Header, para quando estiver em uma determinada página aparecer links que realmente faça sentido para aquela página no Header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,56 +1,121 @@
-import { Link, useLocation } from "react-router-dom";
-import { FiHeart, FiShoppingCart } from "react-icons/fi";
+import { Link, useLocation, useMatch } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
-import logo from '../../assets/logopet.png';
+
+import { FiHeart, FiShoppingCart } from "react-icons/fi";
 import { FaBars } from "react-icons/fa";
+import logo from '../../assets/logopet.png';
 
 export function Header() {
-  
-  const {cartAmount} = useContext(CartContext);
+  const { cartAmount } = useContext(CartContext);
+
   const location = useLocation();
+  const matchHome = useMatch('/');
+  const matchCart = useMatch('/carrinho');
+  const matchFavorites = useMatch('/favoritos');
+  const matchProductDetails = useMatch('/produto/:id');
+  
+  // Verifica se a rota atual é uma rota "not found":
+  const isNotFoundPage = !matchHome && 
+                         !matchCart && 
+                         !matchFavorites && 
+                         !matchProductDetails &&
+                         location.pathname !== '/';
+
+
+  function getLinks() {
+    if (isNotFoundPage) {
+      return (
+        <Link to='/' className="hover:bg-slate-400">
+          <h3>HOME</h3>
+        </Link>
+      );
+    }
+
+    if (matchCart) {
+      return (
+        <>
+          <Link to='/' className="hover:bg-slate-400">
+            <h3>HOME</h3>
+          </Link>
+          <Link to='/favoritos' className="hover:bg-slate-400">
+            <h3>FAVORITOS</h3>
+          </Link>
+        </>
+      );
+    }
+
+    if (matchFavorites) {
+      return (
+        <>
+          <Link to='/' className="hover:bg-slate-400">
+            <h3>HOME</h3>
+          </Link>
+          <Link to='/carrinho' className="hover:bg-slate-400">
+            <h3>CARRINHO</h3>
+          </Link>
+        </>
+      );
+    }
+
+    if (matchProductDetails) {
+      return (
+        <>
+          <Link to='/' className="hover:bg-slate-400">
+            <h3>HOME</h3>
+          </Link>
+          <Link to='/carrinho' className="hover:bg-slate-400">
+            <h3>CARRINHO</h3>
+          </Link>
+          <Link to='/favoritos' className="hover:bg-slate-400">
+            <h3>FAVORITOS</h3>
+          </Link>
+        </>
+      );
+    }
+
+    // Links para a página Home
+    if (matchHome) {
+      return (
+        <>
+          <Link to='#about' target="_parent" className="hover:bg-slate-400">
+            <h3>SOBRE</h3>
+          </Link>
+          <Link to='#services' target="_parent" className="hover:bg-slate-400">
+            <h3>SERVIÇOS</h3>
+          </Link>
+          <Link to='#products' target="_parent" className="hover:bg-slate-400">
+            <h3>LOJA</h3>
+          </Link>
+          <Link to='#feedback' target="_parent" className="hover:bg-slate-400">
+            <h3>DEPOIMENTOS</h3>
+          </Link>
+        </>
+      );
+    }
+
+    // retrun null; garante que a função getLinks sempre retorna um valor, mesmo que nunhuma das consdições anteriores seja atendida
+    return null;
+
+  };
 
   return (
     <header className="w-full px-4 bg-slate-300">
       <nav className="flex items-center max-w-7xl h-24 justify-between px-5 mx-auto select-none">
         <Link to="/" className="font-bold text-2xl flex justify-center items-center">
-          <img className="max-h-24 max-w-24" src={logo} alt="" />
+          <img className="max-h-24 max-w-24" src={logo} alt="Logo" />
           <span className="hidden md:block">PetShopDev</span>
         </Link>
 
-        {location.pathname === '/carrinho' ? (
-          <div className="gap-3 text-normal hidden sm:flex md:text-lg font-semibold text-slate-700">
-            <Link to='/' className="hover:bg-slate-400">
-              <h3>HOME</h3>
-            </Link>
-            <Link to='/favorites' className="hover:bg-slate-400">
-              <h3>FAVORITOS</h3>
-            </Link>
-          </div>
-        ) : (
-          <div className="gap-3 text-normal hidden sm:flex md:text-lg font-semibold text-slate-700">
-            <Link to='#about' target="_parent" className="hover:bg-slate-400">
-              <h3>SOBRE</h3>
-            </Link>
-            <Link to='#services' target="_parent" className="hover:bg-slate-400">
-              <h3>SERVIÇOS</h3>
-            </Link>
-            <Link to='#products' target="_parent" className="hover:bg-slate-400">
-              <h3>LOJA</h3>
-            </Link>
-            <Link to='#feedback' target="_parent" className="hover:bg-slate-400">
-              <h3>DEPOIMENTOS</h3>
-            </Link>
-          </div>
-        )}
+        <div className="gap-3 text-normal hidden sm:flex md:text-lg font-semibold text-slate-700">
+          {getLinks()}
+        </div>
 
         <div className="flex gap-3">
           <FaBars size={24} className="sm:hidden" />
-
           <Link to='' className="relative">
             <FiHeart size={24} color="#121212" />
           </Link>
-          
           <Link to="/carrinho" className="relative">
             <FiShoppingCart size={24} color="#121212" />
             {cartAmount > 0 && (
@@ -61,8 +126,7 @@ export function Header() {
           </Link>
         </div>
 
-
       </nav>
     </header>
-  )
+  );
 }
